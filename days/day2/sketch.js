b65e8daf9dd2adb48f1fda12651415827ddc73a9
Yannick <yannick.lansink@live.nl>
2021-07-09 19:22:09 +0200

    day 2 made canvas dynamic content with p5.js

--- days/day2/sketch.js
+++ days/day2/sketch.js
@@ -1,4 +1,4 @@
-
+/*
 let bubbles = [];
 
 function setup() {
@@ -32,5 +32,39 @@ function Bubble(x, y) {
 		this.x = this.x + random(01, 1);
 		this.y = this.y + random(01, 1);
 	}
+}
+*/
+
+// next function setup
+let numberOfClicks = 0
+let bgcolor;
+let x;
+let y;
+
+function setup() {
+	canvas = createCanvas(500, 500);
+	canvas.position(400, 200);
+	h1 = createElement("h1", "I dare you to click the page...")
+	bgcolor = color(200);
+	x = color(50);
+	y = color(255);
+}
+
+function mousePressed() {
+	let arrayOfWords = ["Well done!", "You are awesome! :)", "Do it again!"];
+	let i = Math.floor(Math.random() * 3);
+	h1.html(arrayOfWords[i]);
+	numberOfClicks++
+	createP("Clicked the page: " + numberOfClicks + " times.");
+	bgcolor = color(random(255));
+	x = color(random(255));
+	y = color(random(255));
+}
+
+function draw() {
+	background(bgcolor)
+	ellipse(50, 50, 80, 80);
+	rect(100, 100, 50, 50);
+	fill(50, 50, 100);
 }
 
