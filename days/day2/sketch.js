3c85b2571bf50635c6f52ef9efd7b69bb4240435
Yannick <yannick.lansink@live.nl>
2021-07-09 13:15:00 +0200

    q

--- days/day2/sketch.js
+++ days/day2/sketch.js
@@ -1,8 +1,36 @@
+
+let bubbles = [];
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// put setup code here
+	createCanvas(600, 400);
+	for (let i =0; i < 10; i++) {
+		const x = random(width);
+		const y = random(height);
+		bubbles.push(new Bubble(x, y));
+	}
 }
 
 function draw() {
-	background(0, 255, 0);
+	background(0);
+	for (let i = 0; i < bubbles.length; i++) {
+		bubbles[i].move();
+		bubbles[i].display();
+	}
 }
+
+function Bubble(x, y) {
+	this.x = x;
+	this.y = y;
+
+	this.display = function() {
+		stroke(255);
+		fill(255, 150);
+		ellipse(this.x, this.y, 48, 48);
+	}
+
+	this.move = function() {
+		this.x = this.x + random(01, 1);
+		this.y = this.y + random(01, 1);
+	}
+}
+
